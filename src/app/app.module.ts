68efce67a10d4f7235011fbb5148048cd696d610
mortizo <mortizo@ups.edu.ec>
2022-08-02 14:52:29 -0500

    Creación de 2 componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
